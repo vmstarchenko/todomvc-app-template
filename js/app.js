29ddbba0112e443853ae860599657cb524705ace
vmstarchenko <vmstarchenko@edu.hse.ru>
2016-11-12 19:38:53 +0300

    add TemplatesStorage class

--- js/app.js
+++ js/app.js
@@ -1,11 +1,10 @@
-import { TemplatesStorage } from "./libs/TemplatesStorage.js";
+import { guestTemplatesStorage } from "./libs/TemplatesStorage.js";
 
 (function(window) {
   'use strict';
 
   // Write npm run watch-js to start coding
-  let storage = new TemplatesStorage();
-  console.log(storage);
-  console.log(NODE_ENV == 'development');
+
+  console.log(guestTemplatesStorage.render('test', {a: 1}));
 
 })(window);
--- js/libs/TemplatesStorage.js
+++ js/libs/TemplatesStorage.js
@@ -1,3 +1,5 @@
+/*************** TemplatesStorage ***************/
+
 class TemplatesStorage {
   /**
    * Save templates here.
@@ -9,15 +11,39 @@ class TemplatesStorage {
 
   constructor(defaultContext={}, templates={}) {
     // debugger;
-    this.context = defaultContext;
+    this.defaultContext = defaultContext;
     this.storage = templates;
   }
 
   render(templateName, context) {
+    context = Object.assign({}, context, this.defaultContext);
     return this.storage[templateName](context);
+  }
+
+  /**
+   * template - function-renderer. takes context
+   */
+  addTemplate(templateName, template) {
+    this.storage[templateName] = template;
   }
 }
 
+/*************** Templates ***************
+ *
+ * Tip: always set default value for context
+ *
+ *****************************************/
+
+function template_Test(c={username: undefined}) {
+  return `hello. it's a simple test. context: ${c}, ${c.username}`;
+}
+
+/*************** Storages ***************/
+let guestTemplatesStorage = new TemplatesStorage({
+  username: 'xxx'
+}, {
+  test: template_Test});
+
 export {
-  TemplatesStorage
+  guestTemplatesStorage
 };
